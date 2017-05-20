2c170456ae46e866ff3cf9787d4507910840c71f
Mitja358 <mitja.bernjak@outlook.com>
2017-05-20 15:19:18 +0200

    Dodan service provider za GET (restavracije)

--- Client/src/pages/home/home.ts
+++ Client/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PrikazRestavracij } from '../../providers/prikaz-restavracij';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  restaurantsList = [];
 
+  constructor(private prikazRestavracij: PrikazRestavracij) {
+    this.getRestaurants();
   }
-
+  getRestaurants() {
+    this.prikazRestavracij.getRestaurants().subscribe(data => this.restaurantsList = data);
+  }
 }
